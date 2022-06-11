68947dcf35e3305ae2cb5b391c29fb222319cc10
Vishal <vssule@mitaoe.ac.in>
2022-06-11 09:06:47 +0530

    code

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,Total;
+    cout<<"Enter the First Number: ";
+    cin>>a;
+    cout<< "Enter the Second Number: ";
+    cin>>b;
+    Total=a+b;
+    cout<<"This is the Sum of the "<<a<<" and "<<b<<" :"<<Total;
+    return 0;
+}
